f2930c72f25583c0a66c30f4a370a32ddecbd68b
noa <chardon@post.bgu.ac.il>
2024-05-27 16:07:43 +0300

    add tabs to guess page

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const triggerTabList = document.querySelectorAll('#myTab button')
-triggerTabList.forEach(triggerEl => {
-    const tabTrigger = new bootstrap.Tab(triggerEl)
-
-    triggerEl.addEventListener('click', event => {
-        event.preventDefault()
-        tabTrigger.show()
-    })
-})
+// const triggerTabList = document.querySelectorAll('#myTab button')
+// triggerTabList.forEach(triggerEl => {
+//     const tabTrigger = new bootstrap.Tab(triggerEl)
+//
+//     triggerEl.addEventListener('click', event => {
+//         event.preventDefault()
+//         tabTrigger.show()
+//     })
+// })
