216b31842aa739518502d7801b08b0b4a1d07494
nono20794 <chardon@post.bgu.ac.il>
2024-05-28 23:47:14 +0300

    fix group changing img

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+document.addEventListener('DOMContentLoaded', function() {
+    const selectElements = document.querySelectorAll('select.form-select');
 
-myModal.addEventListener('shown.bs.modal', () => {
-    myInput.focus()
-})
+    selectElements.forEach(select => {
+        select.addEventListener('change', function() {
+            const selectedValue = this.value;
+            const imgElement = this.parentElement.querySelector('img.img-fluid');
+            updateImage(imgElement, selectedValue);
+        });
+    });
+});
+
+function updateImage(imgElement, selectedValue) {
+    console.log(imgElement, selectedValue);
+    let flagSrc;
+    switch (selectedValue) {
+        case 'germany':
+            flagSrc = 'flags/Germany.svg';
+            break;
+        case 'switzerland':
+            flagSrc = 'flags/Switzerland.svg';
+            break;
+        case 'hungary':
+            flagSrc = 'flags/Hungary.svg';
+            break;
+        case 'scotland':
+            flagSrc = 'flags/Scotland.svg';
+            break;
+        default:
+            flagSrc = '';
+    }
+    imgElement.src = flagSrc;
+}
